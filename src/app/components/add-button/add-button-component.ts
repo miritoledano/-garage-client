465241ded06b4c5c0ae1bed3705f5c5feb7a4fac
miritoledano <mt0548553246@gmail.com>
2025-12-02 19:56:13 +0200

    added AddButton component with loading indicator support

--- src/app/components/add-button/add-button-component.ts
+++ src/app/components/add-button/add-button-component.ts
@@ -12,10 +12,13 @@ import { CommonModule } from '@angular/common';
 })
 export class GarageAddButtonComponent {
   @Input() selectedGarages: Garage[] = [];
+    //  משדר אירוע להורה כשהכפתור נלחץ → מפעיל את פעולת ההוספה למסד
+
   @Output() addClicked = new EventEmitter<void>();
+  // מצב טעינה מוגדר ברגע שנילחץ יהפוך לאמת
   @Input() loading: boolean = false;
 
-
+// נשלח לאבא פונקצית הוספה כדי שיוסיף לדאהבייס
   onClick() {
     this.addClicked.emit();
   }
